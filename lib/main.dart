0002820637d938f4fb5f3c9af377ae6f31a7a222
NasserNengm <nasserabdelhay@icloud.com>
2024-12-16 17:57:59 +0200

    add object to lib

--- lib/main.dart
+++ lib/main.dart
@@ -3,5 +3,5 @@ import 'session2.dart';
 void main() {
   Human h1 = Human(name: "Nasser", weight: 70, tall: 175);
 
-  // Human h2 = Human("Hamza", 30, 150);
+  Human h2 = Human(name: "Hamza", weight: 150, tall: 180);
 }
